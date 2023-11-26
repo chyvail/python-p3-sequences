f8cebb12ae9d49363ff690dfc9b6412a895c73a3
joshua chivile <joshuachivile@gmail.com>
2023-11-26 17:29:00 +0300

    Fibonacci sequence done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
+# Formula for fibonacci is f(n) = f(n-1) + f(n-2)
 def print_fibonacci(length):
-    pass
+    num1 =0
+    num2=1
+    fib=[num1,num2]
+    for i in range(length):
+        sum = num1 + num2
+        num1 = num2
+        num2 = sum
+        fib.append(num2)
+    print(fib[:length])
+
+print_fibonacci(9)
